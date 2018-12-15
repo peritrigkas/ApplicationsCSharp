d012ff9eb6c3f18245525189760fe3841359cd36
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-12-15 02:53:17 +0000

    String Comparison

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -11,7 +11,28 @@ namespace SimpleCalculator
         static void Main(string[] args)
         {
             string someText = " Here is some Text";
-            int number = 10; 
+            int number = 10;
+            double anotherNumber = 10.9;
+            bool isOrdered = true;
+            string input = Console.ReadLine();
+            var anything = 10.9;
+            Console.WriteLine(someText);
+            Console.WriteLine(number);
+            Console.WriteLine(anotherNumber);
+            Console.WriteLine(isOrdered);
+            Console.WriteLine(input);
+            Console.WriteLine(anything);
+            int c;
+            int.TryParse(input, out c);
+
+            Console.WriteLine(10 - 9 + c);
+
+
+            string someTexts = " Here is some text";
+            string someOtherText = "Here is some other text";
+
+           bool isEqueal = someTexts.Equals(someOtherText, StringComparison.Ordinal); // makes char by char comparison
+                                                                                      // currentCulture takes into consideration user's location
         }
     }
 }
