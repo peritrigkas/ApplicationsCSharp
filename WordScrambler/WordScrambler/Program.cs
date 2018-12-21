237f7aab5d486468f24e56a25164056e1ed4961d
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-12-21 18:46:57 +0000

    Cleaned Main. Separated Input File options in a new class

--- WordScrambler/WordScrambler/Program.cs
+++ WordScrambler/WordScrambler/Program.cs
@@ -10,34 +10,11 @@ namespace WordScrambler
     {
         static void Main(string[] args)
         {
-            bool continueWordUnscrambler = true;
 
-            do
-            {
-                Console.WriteLine("Please enter the Option - F for file and M for Manual");
-                var option = Console.ReadLine() ?? string.Empty;
+            StartingMenu InputOption = new StartingMenu();
 
-                switch (option.ToUpper())
-                {
-                    case "F":
-                        Console.WriteLine("Enter scrambled words file name :");
-                        ExecuteScrambledWord.InFileScenario();
-                        break;
-                    case "M":
-                        Console.WriteLine("Enter scrambled words manually :");
-                        ExecuteScrambledWord.ManualScenario();
-                        break;
-                    default:
-                        Console.WriteLine("Option is not recognised");
-                        break;
-                }
-                var continueWordUnscrambleDecision = string.Empty;
-                do
-                {
-                    Console.WriteLine("Do you want to continue the program?");
-
-                } while (!continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
-            } while ();
+            InputOption.FileInputOption();
+        
 
          
         }
---
+++ WordScrambler/WordScrambler/StartingMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordScrambler
+{
+    class StartingMenu
+    {
+
+        public void FileInputOption()
+        {
+            bool continueWordUnscrambler = true;
+
+            do
+            {
+                Console.WriteLine("Please enter the Option - F for file and M for Manual");
+                var option = Console.ReadLine() ?? string.Empty;
+
+                switch (option.ToUpper())
+                {
+                    case "F":
+                        Console.WriteLine("Enter scrambled words file name :");
+                        ExecuteScrambledWord.InFileScenario();
+                        break;
+                    case "M":
+                        Console.WriteLine("Enter scrambled words manually :");
+                        ExecuteScrambledWord.ManualScenario();
+                        break;
+                    default:
+                        Console.WriteLine("Option is not recognised");
+                        break;
+                }
+
+                var continueWordUnscrambleDecision = string.Empty;
+
+                do
+                {
+                    Console.WriteLine("Do you want to continue the program?");
+                    continueWordUnscrambleDecision = (Console.ReadLine() ?? string.Empty);
+
+                } while (!continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase) && !continueWordUnscrambleDecision.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+
+                continueWordUnscrambler = continueWordUnscrambleDecision.Equals("Y", StringComparison.OrdinalIgnoreCase);
+
+
+
+            } while (continueWordUnscrambler);
+        }
+    }
+}
