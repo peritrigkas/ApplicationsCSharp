207bfc6b0f23d367f53f3fb1c926853090181384
peritrigkas <30832294+peritrigkas@users.noreply.github.com>
2018-12-22 12:22:34 +0000

    Finished

--- WordScrambler/WordScrambler/WordMatcher.cs
+++ WordScrambler/WordScrambler/WordMatcher.cs
@@ -3,24 +3,50 @@ using System.Collections.Generic;
 
 namespace WordScrambler
 {
-     class WordMatcher
+    class WordMatcher
     {
         public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
         {
             var matchedWords = new List<MatchedWord>();
 
-            foreach(var scrambleddWord in scrambledWords)
+            foreach (var scrambledWord in scrambledWords)
             {
-                foreach(var word in wordList)
+                foreach (var word in wordList)
                 {
-                    if(scrambleddWord.Equals(word, StringComparison.OrdinalIgnoreCase))
+                    if (scrambledWord.Equals(word, StringComparison.OrdinalIgnoreCase))
                     {
-                        matchedWords.Add();
+                        matchedWords.Add(BuildmatchedWord(scrambledWord, word));
+                    }
+                    else
+                    {
+                        var scrambredWordArray = scrambledWord.ToCharArray();
+                        var wordArray = word.ToCharArray();
+
+                        Array.Sort(scrambredWordArray);
+                        Array.Sort(wordArray);
+
+                        var sortedScrambledWord = new string(scrambredWordArray);
+                        var sortedWord = new string(wordArray);
+
+                        if (sortedScrambledWord.Equals(sortedWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            matchedWords.Add(BuildmatchedWord(scrambledWord, word));
+                        }
                     }
                 }
             }
 
             return matchedWords;
+        }
+
+        private MatchedWord BuildmatchedWord(string scrambledWord, string word)
+        {
+            MatchedWord matchedWord = new MatchedWord
+            {
+                ScrambledWord = scrambledWord,
+                Word = word
+            };
+            return matchedWord;
         }
     }
 }
